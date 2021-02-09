07982dab82df547abac6bce5906579a9919accb5
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2021-02-09 09:12:58 -0300

    removendo bibliotecas nao utilizadas

--- weather/src/main/java/br/com/api/weather/WeatherApplication.java
+++ weather/src/main/java/br/com/api/weather/WeatherApplication.java
@@ -2,10 +2,8 @@ package br.com.api.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "br.com.api.weather.model")
 public class WeatherApplication {
 
 	public static void main(String[] args) {
